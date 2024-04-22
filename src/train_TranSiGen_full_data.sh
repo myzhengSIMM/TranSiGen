05995f1aef99fcc3683edc6289130a4ec3e2bf80
myzheng-SIMM <149573757+myzheng-SIMM@users.noreply.github.com>
2024-04-22 09:38:06 +0800

    Add files via upload

---
+++ src/train_TranSiGen_full_data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+python train_TranSiGen_full_data.py --data_path ../data/LINCS2020/data_example/processed_data_id.h5
+                                    --molecule_path ../data/LINCS2020/idx2smi.pickle
+                                    --molecule_feature KPGT
+                                    --initialization_model pretrain_shRNA
+                                    --split_data_type smiles_split
+                                    --n_epochs 300
+                                    --n_latent 100
+                                    --molecule_feature_embed_dim 400
+                                    --batch_size 64
+                                    --learning_rate 1e-3
+                                    --beta 0.1
+                                    --dropout 0.1
+                                    --weight_decay 1e-5
+                                    --train_flag True
+                                    --eval_metric True
+
+
+
+python prediction.py --model_path ../results/trained_models_164_cell_smiles_split/364039/feature_KPGT_init_pretrain_shRNA/best_model.pt
+                     --data_path ../data/PRISM/screening_compound.csv
+                     --molecule_feature_embed_path ../data/PRISM/KPGT_emb2304.pickle
+                     --cell YAPC
+                     --seed 100
